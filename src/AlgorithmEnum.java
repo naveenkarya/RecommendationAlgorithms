ef9a5d8140f37e58bc427284bb0b85a3d707daf0
navee <naveenkkarya@gmail.com>
2021-11-21 13:24:50 -0800

    Revert "Revert "refactor""
    This reverts commit 65af8c65

--- src/AlgorithmEnum.java
+++ src/AlgorithmEnum.java
@@ -4,17 +4,12 @@ public enum AlgorithmEnum {
     PEARSON_CORRELATION_IUF_ONLY("3", "Pearson-Correlation-With-IUF-Only"),
     PEARSON_CORRELATION_CASE_MOD_ONLY("4", "Pearson-Correlation-With-CaseMod_Only"),
     ITEM_BASED_CF( "5", "Item-Based-Collaborative-Filtering"),
-    EUCLIDEAN_DISTANCE_WITH_LOG_BASE("6", "Euclidean-Distance-With-Log-Base"),
-    EUCLIDEAN_DISTANCE_WITH_JACCARD("7", "Euclidean-Distance-With-Jaccard"),
-    COSINE_SIM_WITH_LOG_BASE("8", "Cosine-Similarity-With-Log-Base"),
-    COSINE_SIM_WITH_JACCARD("9", "Cosine-Similarity-With-Jaccard"),
-    EUCLIDEAN_DISTANCE("10", "Euclidean-Distance"),
-    WEIGHTED_SLOPE_ONE("11", "Weighted Slope-One"),
-    NORMALIZED_EUCLIDEAN_DISTANCE("12", "Normalized-Euclidean-Distance"),
-    BASIC_AVERAGE_ALGORITHM("14", "Basic-Average-Algorithm"),
-    BASIC_AVERAGE_ALGORITHM2("15", "Basic-Average-Algorithm2"),
-    COSINE_SIMILARITY_AND_ITEM_BASED("16", "Cosine Similarity With Item Based"),
-    COSINE_AND_JACCARD("17", "Cosine and Jaccard");
+    USER_AND_ITEMS_AVERAGE("6", "User's and Item's Average"),
+    COSINE_SIMILARITY_AND_ITEM_BASED("7", "Cosine Similarity With Item Based"),
+    NORMALIZED_EUCLIDEAN_DISTANCE("8", "Normalized-Euclidean-Distance"),
+    EUCLIDEAN_DISTANCE_WITH_LOG_BASE("9", "Euclidean-Distance-With-Log-Base"),
+    EUCLIDEAN_DISTANCE_WITH_JACCARD("10", "Euclidean-Distance-With-Jaccard"),
+    WEIGHTED_SLOPE_ONE("11", "Weighted Slope-One");
 
     private final String algoCode;
     private final String algoName;
